d2e4c5698915f725258ad0f3e6bd5bbb0ef37f68
dipankarnag <emailtodipankar@rediffmail.com>
2019-10-06 12:17:47 +0530

    Matgrid Height made dynamic with window resize

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  public innerWidth: any;
+  public innerHeight: any;
+  @ViewChild('topBar') topBar: ElementRef|any;
+  public topBarHeight: any;
+  public matGridRowHeight: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.innerHeight = window.innerHeight;
+    this.innerWidth = window.innerWidth;
+    this.topBarHeight = this.topBar._elementRef.nativeElement.offsetHeight;
+    this.matGridRowHeight = ((this.innerHeight - this.topBarHeight) / 2) - 1;
+    console.log(this.innerWidth, this.innerHeight, this.topBarHeight);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onresize() {
+    this.innerWidth = window.innerWidth;
+    this.innerHeight = window.innerHeight;
+    this.topBarHeight = this.topBar._elementRef.nativeElement.offsetHeight;
+    this.matGridRowHeight = ((this.innerHeight - this.topBarHeight) / 2) - 1;
+    console.log(this.innerWidth, this.innerHeight, this.topBarHeight);
   }
 
 }
