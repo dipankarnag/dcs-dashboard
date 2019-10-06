96af89201b0f7cecb288092eb4bc3beaa6177154
dipankarnag <emailtodipankar@rediffmail.com>
2019-10-06 19:56:55 +0530

    swupdate configured to reload on update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dcs-dashboard';
+
+  constructor(private swUpdate: SwUpdate) {}
+
+  ngOnInit() {
+    this.reloadCache();
+  }
+
+  reloadCache() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('New version available! Would you like to update?')) {
+          window.location.reload();
+        }
+      });
+    }
+  }
 }
