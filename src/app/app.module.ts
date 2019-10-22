7bb715a12865911cef2f144a37db704fd098d768
dipankarnag <emailtodipankar@rediffmail.com>
2019-10-22 16:41:28 +0530

    abundant components removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './modules/material/material.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { BktppComponent } from './components/bktpp/bktpp.component';
-import { KtpsComponent } from './components/ktps/ktps.component';
-import { BtpsComponent } from './components/btps/btps.component';
-import { StpsComponent } from './components/stps/stps.component';
-import { SgtppComponent } from './components/sgtpp/sgtpp.component';
+
 import { LegendComponent } from './components/legend/legend.component';
 import { PlantComponent } from './components/plant/plant.component';
 
@@ -21,11 +17,6 @@ import { PlantComponent } from './components/plant/plant.component';
   declarations: [
     AppComponent,
     routingComponents,
-    BktppComponent,
-    KtpsComponent,
-    BtpsComponent,
-    StpsComponent,
-    SgtppComponent,
     LegendComponent,
     PlantComponent
   ],
