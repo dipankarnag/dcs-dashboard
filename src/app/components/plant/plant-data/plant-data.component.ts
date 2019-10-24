5e29434d1f1ca5c66de596bc84ffb93fd6bce4a5
dipankarnag <emailtodipankar@rediffmail.com>
2019-10-24 15:48:32 +0530

    table style modified

--- src/app/components/plant/plant-data/plant-data.component.ts
+++ src/app/components/plant/plant-data/plant-data.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, HostListener, ViewChild, ElementRef } from '@angular/core';
 import { MatTableDataSource, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
@@ -7,6 +7,9 @@ import { MatTableDataSource, MatDialogRef, MAT_DIALOG_DATA } from '@angular/mate
   styleUrls: ['./plant-data.component.scss']
 })
 export class PlantDataComponent implements OnInit {
+
+  @ViewChild('tableView')
+  public tableView: ElementRef|any;
 
   tagList: MatTableDataSource<any>;
   displayedColumns: string[] = [
@@ -23,12 +26,43 @@ export class PlantDataComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    // console.log('from plant-data', this.passedData);
     this.tagList = new MatTableDataSource(this.passedData.data.tags);
+    this.modifyDataInTable((window.innerWidth < 650) ? 21 : 50);
+    console.log();
   }
 
   onClose() {
     this.dialogRef.close();
+  }
+
+  @HostListener('window:load', ['$event'])
+  @HostListener('window:resize', ['$event'])
+  onrender() {
+    // console.log(window.innerWidth);
+    this.modifyDataInTable((window.innerWidth < 650) ? 21 : 50);
+    // this.tableView._elementRef.nativeElement.childNodes.forEach(item => console.log(item));
+  }
+
+  breakString(str: string, breakLength: number) {
+    return (str.length <= breakLength) ?
+      str : str.substr(0, breakLength) + '\n' + this.breakString(str.substr(breakLength, str.length - breakLength), breakLength);
+  }
+
+  modifyDataInTable(breakLength: number) {
+    const tempTagList = this.tagList.data;
+    // console.log(tempTagList[0].name);
+
+    // tempTagList.forEach(item => {
+    //   tempTagList[tempTagList.indexOf(item)].name = this.breakString(item.name, 10);
+    //   console.log(this.breakString(tempTagList[tempTagList.indexOf(item)].name, 10));
+    // });
+    for (let i = 0; i < tempTagList.length; i++) {
+      tempTagList[i].name = this.breakString(tempTagList[i].name, breakLength);
+    }
+    // console.log(tempTagList);
+
+    // tempTagList.forEach(item => console.log(item.name));
+    this.tagList.data = tempTagList;
   }
 
 }
